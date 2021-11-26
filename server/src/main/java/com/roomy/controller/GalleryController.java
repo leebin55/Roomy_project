3bfba0ad240b59be98ae92c19f7c30ebc1df3290
leebin55 <yublee55@gmail.com>
2021-11-26 20:35:53 +0900

    spring security configuration 설정> 우선 막아놈

--- server/src/main/java/com/roomy/controller/GalleryController.java
+++ server/src/main/java/com/roomy/controller/GalleryController.java
@@ -1,6 +1,7 @@
 package com.roomy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GalleryController {
 
-    @RequestMapping(value = {"/",""},method = RequestMethod.GET)
+    @GetMapping(value = {"/",""})
     public String list(){
-        return "";
+        return "hello";
     }
 }
---
+++ server/src/main/java/com/roomy/controller/MainController.java
@@ -0,0 +1,12 @@
+package com.roomy.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MainController {
+    @GetMapping(value = {"/",""})
+    public String Main(){
+        return "Main Page";
+    }
+}
