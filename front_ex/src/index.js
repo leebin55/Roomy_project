64d9b34627b058c84b0cc13be2a4bfcffe0edfb7
leebin55 <yublee55@gmail.com>
2021-11-26 21:23:55 +0900

    react index.js 에 Route 들 App.js로 옮김

--- front_ex/src/index.js
+++ front_ex/src/index.js
@@ -1,41 +1,15 @@
-import React from "react";
-import { render } from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import Game from "./routes/Game";
-import News from "./routes/News";
-import Feeds from "./components/Feeds";
-import {
-  RoomMain,
-  Board,
-  Gallery,
-  Guest,
-  Todo,
-  Setting,
-} from "./components/room/RoomComps";
-import Room from "./routes/Room";
+import React from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 render(
   <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />}>
-        <Route index element={<Feeds />} />
-        <Route path="game" element={<Game />} />
-        <Route path="news" element={<News />} />
-      </Route>
-      <Route path="/room" element={<Room />}>
-        <Route index element={<RoomMain />} />
-        <Route path="board" element={<Board />} />
-        <Route path="todo" element={<Todo />} />
-        <Route path="gallery" element={<Gallery />} />
-        <Route path="guest" element={<Guest />} />
-        <Route path="setting" element={<Setting />} />
-      </Route>
-    </Routes>
+    <App />
   </BrowserRouter>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
