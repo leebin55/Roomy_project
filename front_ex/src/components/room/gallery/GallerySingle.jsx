547723b61f1dc413a6845e7164d8288184fe9eb6
leebin55 <yublee55@gmail.com>
2021-12-01 17:42:50 +0900

    gallery imagelist

--- front_ex/src/components/room/gallery/GallerySingle.jsx
+++ front_ex/src/components/room/gallery/GallerySingle.jsx
@@ -1,29 +1,49 @@
-import React, { useState } from 'react';
-import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
-import FavoriteIcon from '@mui/icons-material/Favorite';
+import React, { useState } from "react";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import FavoriteIcon from "@mui/icons-material/Favorite";
+import ImageListItem from "@mui/material/ImageListItem";
+import ImageListItemBar from "@mui/material/ImageListItemBar";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-function GallerySingle({ gallery }) {
+function GallerySingle({ gallery, index }) {
   const [isLike, setIsLike] = useState(false);
 
   const likeClick = (event) => {
-    event.preventDefault();
     setIsLike(!isLike);
   };
+  const clickImg = (event) => {
+    axios
+      .get(`http://localhost:8080/room/gallery?board_seq=${event.target.alt}`)
+      .then((res) => {
+        if (res.data === "ok") {
+        }
+      });
+  };
   return (
-    <div className="gallery-list" key={gallery.board_seq}>
-      <div>
-        <input type="checkbox" />
-        <h3>{gallery.board_title}</h3>
-        <p>{gallery.board_create_at}</p>
-      </div>
-      <p>{gallery.board_content}</p>
-      <div>
-        <span onClick={likeClick}>
-          {isLike ? <FavoriteIcon /> : <FavoriteBorderIcon />}
-          {gallery.board_like}
-        </span>
-      </div>
-    </div>
+    <ImageListItem key={gallery.board_seq}>
+      <img
+        // src={`${item.img}?w=248&fit=crop&auto=format`}
+        // srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
+        // alt={item.title}
+        // loading="lazy"
+        alt={gallery.board_seq}
+        onClick={clickImg}
+      />
+      <ImageListItemBar
+        title={gallery.board_title}
+        subtitle={<span>by: {gallery.board_create_at}</span>}
+        position="below"
+      />
+      <span onClick={likeClick}>
+        {isLike ? (
+          <FavoriteIcon className="gallery-like" />
+        ) : (
+          <FavoriteBorderIcon className="gallery-like" />
+        )}
+        {gallery.board_like}
+      </span>
+    </ImageListItem>
   );
 }
 
--- front_ex/src/components/room/gallery/GalleryWrite.jsx
+++ front_ex/src/components/room/gallery/GalleryWrite.jsx
@@ -1,13 +1,56 @@
-import React from 'react';
+import React, { useState } from "react";
+import moment from "moment";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 function GalleryWrite() {
+  const navigate = useNavigate();
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
+
+  const titleChange = (evnet) => {
+    setTitle(evnet.target.value);
+  };
+
+  const writeSubmit = async () => {
+    try {
+      await axios
+        .post("http://localhost:8080/room/gallery/write", {
+          board_user_seq: 1,
+          board_title: title,
+          board_content: content,
+          board_create_at: moment().format("YYYY-MM-DD HH:mm:ss"),
+          board_code: 1,
+        })
+        .then((res) => {
+          if (res.data === "ok") {
+            navigate("/room/gallery");
+          }
+        });
+    } catch (error) {
+      throw error;
+    }
+  };
+
   return (
     <div>
       <h1>gallery write</h1>
-      <div>
-        <label>Title : </label>
-        <input />
-      </div>
+      <form>
+        <div>
+          <label>Title : </label>
+          <input name="board_title" value={title} onChange={titleChange} />
+        </div>
+        <input
+          name="board_content"
+          value={content}
+          onChange={(event) => {
+            setContent(event.target.value);
+          }}
+        />
+        <button type="button" onClick={writeSubmit}>
+          등록
+        </button>
+      </form>
     </div>
   );
 }
