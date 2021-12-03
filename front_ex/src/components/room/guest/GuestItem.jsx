6efa845071e078708c1c97896f179f909916380a
choiseonyoung <bviibb@naver.com>
2021-12-03 17:52:38 +0900

    오류파티

--- front_ex/src/components/room/guest/GuestItem.jsx
+++ front_ex/src/components/room/guest/GuestItem.jsx
@@ -1,77 +1,27 @@
-import React, { useState } from "react";
+// import React, { useState } from "react";
 
-function GuestItem({ data, fetchList, content }) {
-  const [guest_update, setGuest_update] = useState(false);
+// function GuestItem({ data, fetchList }) {
 
-  const guestDelete = async () => {
-    if (window.confirm("방명록을 삭제하시겠습니까?")) {
-      //   alert(data.guest_seq);
-      await fetch(`http://localhost:8080/room/guest/${data.guest_seq}`, {
-        method: "DELETE",
-      }).then((res) => {
-        if (res?.ok) {
-          alert("삭제되었습니다");
-          fetchList();
-        }
-      });
-    }
-  };
+//   // const guestUpdate = async () => {
+//   //   setUpdating(!updating);
+//   //   // if (content === "") {
+//   //   //   alert("방명록을 입력하세요");
+//   //   //   return;
+//   //   // }
+//   //   await fetch("http://localhost:8080/room/guest", {
+//   //     method: "PUT",
+//   //     headers: {
+//   //       "Content-Type": "application/json",
+//   //     },
+//   //     body: JSON.stringify({
+//   //       guest_content: update_content,
+//   //     }),
+//   //   });
+//   // };
 
-  const guestUpdate = async () => {
-    setGuest_update(!guest_update);
-    if (content === "") {
-      alert("방명록을 입력하세요");
-      return;
-    }
-    await fetch("http://localhost:8080/room/guest", {
-      method: "PUT",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        guest_content: content,
-      }),
-    });
-  };
+//   return (
 
-  return (
-    <div className="guest_item_box">
-      <section className="guest_head">
-        <p>
-          <span>NO.</span>
-          {data.guest_seq}
-        </p>
-        <p>{data.guest_writer_name}</p>
-        <p>&#127968;</p>
-        <p>{data.guest_create_at}</p>
-        <p className="guest_list_private">
-          {data.guest_private ? "공개글로 전환" : "비밀글로 전환"}
-        </p>
-        <p className="guest_list_update" onClick={guestUpdate}>
-          {guest_update ? "등록" : "수정"}
-        </p>
-        <p className="guest_list_delete" onClick={guestDelete}>
-          삭제
-        </p>
-      </section>
-      <section className="guest_body">
-        <div className="guest_img"></div>
-        <div className="guest_content">
-          {guest_update ? (
-            <textarea
-              className="guest_update_content"
-              defaultValue={data.guest_content}
-            ></textarea>
-          ) : (
-            <>
-              {data.guest_private ? "🔒 " : ""}
-              {data.guest_content}
-            </>
-          )}
-        </div>
-      </section>
-    </div>
-  );
-}
+//   );
+// }
 
-export default GuestItem;
+// export default GuestItem;
--- front_ex/src/components/room/guest/GuestMain.jsx
+++ front_ex/src/components/room/guest/GuestMain.jsx
@@ -3,22 +3,36 @@ import "../../../css/Guest.css";
 import GuestItem from "./GuestItem";
 
 function GuestMain() {
-  const [content, setContent] = useState(""); // 입력한 방명록 작성 내용
+  const [insert_content, setInsert_content] = useState(""); // 입력한 방명록 작성 내용
   const [pp, setPp] = useState(false); // 입력한 방명록 공개여부
   const [guest_list, setGuest_list] = useState([]);
+  const [updating, setUpdating] = useState(false);
+  const [update_content, setUpdate_content] = useState("");
+
+  const fetchList = async () => {
+    const response = await fetch("http://localhost:8080/room/guest");
+    const data = await response.json();
+    setGuest_list(data.reverse());
+  };
+
+  useEffect(fetchList, [setGuest_list]);
 
   const guestPrivate = () => {
     setPp((pp) => !pp);
   };
 
-  const onChange = (e) => {
+  const onInsertChange = (e) => {
     // const g_content = e.target.value;
     // setContent({ ...content, g_content });
-    setContent(e.target.value);
+    setInsert_content(e.target.value);
+  };
+
+  const onUpdateChange = (e) => {
+    setUpdate_content(e.target.value);
   };
 
   const guestInsert = async () => {
-    if (content === "") {
+    if (insert_content === "") {
       alert("방명록을 입력하세요");
       return;
     }
@@ -28,23 +42,25 @@ function GuestMain() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        guest_content: content,
+        guest_content: insert_content,
         guest_private: pp,
       }),
     });
     setPp(false);
-    setContent("");
+    setInsert_content("");
     fetchList();
   };
 
-  useEffect(() => {
-    fetchList();
-  }, []);
-
-  const fetchList = async () => {
-    const response = await fetch("http://localhost:8080/room/guest");
-    const data = await response.json();
-    setGuest_list(data.reverse());
+  const guestDelete = async (e) => {
+    if (window.confirm("방명록을 삭제하시겠습니까?")) {
+      await fetch(`http://localhost:8080/room/guest/${e.target.dataset.id}`, {
+        method: "DELETE",
+      }).then((res) => {
+        if (res?.ok) {
+          alert("삭제되었습니다");
+        }
+      });
+    }
   };
 
   return (
@@ -52,9 +68,51 @@ function GuestMain() {
       <section className="guest_list">
         {guest_list.length > 0 ? (
           guest_list.map((item) => {
-            return (
-              <GuestItem data={item} fetchList={fetchList} content={content} />
-            );
+            <div className="guest_item_box">
+              <section className="guest_head">
+                <p>
+                  <span>NO.</span>
+                  {item.guest_seq}
+                </p>
+                <p>{item.guest_writer_name}</p>
+                <p>&#127968;</p>
+                <p>{item.guest_create_at}</p>
+                <p className="guest_list_private">
+                  {item.guest_private ? "공개글로 전환" : "비밀글로 전환"}
+                </p>
+                <p
+                  className="guest_list_update"
+                  onClick={() => setUpdating(!updating)}
+                >
+                  {updating ? "등록" : "수정"}
+                </p>
+                <p
+                  className="guest_list_delete"
+                  onClick={guestDelete}
+                  data-id={item.id}
+                >
+                  삭제
+                </p>
+              </section>
+              <section className="guest_body">
+                <div className="guest_img"></div>
+                <div className="guest_content">
+                  {updating ? (
+                    <textarea
+                      className="guest_update_content"
+                      defaultValue={item.guest_content}
+                      value={update_content}
+                      onChange={onUpdateChange}
+                    ></textarea>
+                  ) : (
+                    <>
+                      {item.guest_private ? "🔒 " : ""}
+                      {item.guest_content}
+                    </>
+                  )}
+                </div>
+              </section>
+            </div>;
           })
         ) : (
           <div className="guest_item_box">아직 등록된 방명록이 없습니다</div>
@@ -66,8 +124,8 @@ function GuestMain() {
         </div>
         <textarea
           name="guest_write_content"
-          value={content}
-          onChange={onChange}
+          value={insert_content}
+          onChange={onInsertChange}
         />
         <button onClick={guestInsert}>등록</button>
       </section>
