43742f15551e83a72e49b495474abfc7e352ea84
leebin55 <yublee55@gmail.com>
2021-12-08 07:58:09 +0900

    selectAll변경

--- front_ex/src/components/room/gallery/GalleryUpdate.jsx
+++ front_ex/src/components/room/gallery/GalleryUpdate.jsx
@@ -6,11 +6,18 @@ import Editor from './Editor';
 import { useNavigate } from 'react-router-dom';
 import { useGalleryContext } from '../../../context/GalleryContextProvider';
 import axios from 'axios';
-import GalleryDetail from './GalleryDetail';
+import moment from 'moment';
 
 function GalleryUpdate({ boardSeq }) {
-  const { galleryInfo, setGalleryInfo, setContent, content, setTitle, title } =
-    useGalleryContext();
+  const {
+    galleryInfo,
+    setGalleryInfo,
+    setContent,
+    setTitle,
+    title,
+    content,
+    setIsUpdate,
+  } = useGalleryContext();
 
   useEffect(() => {
     viewGalleryInfo();
@@ -18,14 +25,14 @@ function GalleryUpdate({ boardSeq }) {
 
   const viewGalleryInfo = async () => {
     try {
-      // userParam
       await axios
         .get(`http://localhost:8080/room/gallery/detail?board_seq=${boardSeq}`)
         .then((res) => {
           if (res.status === 200) {
-            const gallery = res.data;
-            setContent(gallery.boardContent);
-            setTitle(gallery.boardTitle);
+            console.log(res.data);
+            setGalleryInfo(res.data);
+            setContent(galleryInfo.boardContent);
+            setTitle(galleryInfo.boardTitle);
           }
         });
     } catch (error) {
@@ -34,11 +41,26 @@ function GalleryUpdate({ boardSeq }) {
     }
   };
 
-  const dataSetting = () => {
-    setTitle(GalleryDetail.boar);
+  const updateClick = () => {
+    setGalleryInfo({
+      ...galleryInfo,
+      boardContent: content,
+      boardTitle: title,
+      boardUpdateAt: moment().format('YYYY-MM-DD HH:mm'),
+    });
   };
   const navigate = useNavigate();
 
+  const updateGallery = async () => {
+    try {
+      axios
+        .put('http://localhost:8080/room/gallery/update', galleryInfo)
+        .then((res) => {});
+    } catch (error) {
+      alert('갤러리 수정 실패');
+      throw error;
+    }
+  };
   return (
     <div>
       <div>
@@ -46,13 +68,18 @@ function GalleryUpdate({ boardSeq }) {
         <input name="board_title" value={title} />
         <button
           onClick={() => {
+            setIsUpdate(false);
             navigate('/room/gallery');
           }}
         >
           <ArrowBackIcon />
           뒤로
         </button>
-        <button>
+        <button
+          onClick={() => {
+            updateClick();
+          }}
+        >
           <CheckCircleIcon />
           완료
         </button>
