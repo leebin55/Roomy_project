a9704bb6d53bffa15f826458adc7b2be021e2469
leebin55 <yublee55@gmail.com>
2021-12-09 09:22:00 +0900

    설정부분수정

--- front_ex/src/components/room/Setting.jsx
+++ front_ex/src/components/room/Setting.jsx
@@ -1,9 +1,33 @@
-import React from "react";
+import React from 'react';
+import TextField from '@mui/material/TextField';
 
 function Setting() {
   return (
     <div>
-      <p>설정입니다</p>
+      <h1>설정</h1>
+      <div>
+        <div>
+          <p>사진</p>
+        </div>
+        <TextField
+          id="room-name"
+          defaultValue="Room의 이름"
+          helperText="Room의 이름"
+          margin="normal"
+        />
+      </div>
+      <div>
+        <TextField
+          id="outlined-multiline-static"
+          multiline
+          rows={10}
+          defaultValue="Room 소개글"
+          helperText="Room 소개글"
+          margin="normal"
+          fullWidth="true"
+        />
+      </div>
+      <button>수정</button>
     </div>
   );
 }
--- server/src/main/java/com/roomy/service/impl/RoomServiceImpl.java
+++ server/src/main/java/com/roomy/service/impl/RoomServiceImpl.java
@@ -2,6 +2,7 @@ package com.roomy.service.impl;
 
 
 import com.roomy.model.RoomVO;
+import com.roomy.repository.RoomRepository;
 import com.roomy.service.RoomService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -11,27 +12,35 @@ import java.util.List;
 @Slf4j
 @Service
 public class RoomServiceImpl implements RoomService {
-    @Override
-    public List<RoomVO> selectAll() {
-        return null;
+
+    private final RoomRepository roomRepository;
+
+    public RoomServiceImpl(RoomRepository roomRepository) {
+        this.roomRepository = roomRepository;
     }
 
     @Override
-    public RoomVO findById(Long aLong) {
-        return null;
+    public List<RoomVO> selectAll() {
+        return roomRepository.findAll();
+    }
+
+    @Override
+    public RoomVO findById(Long roomSeq) {
+        return roomRepository.findById(roomSeq).get();
     }
 
     // 회원 가입을 하면 자동으로 룸도 생성되게
     @Override
     public void insert(RoomVO roomVO) {
-
+        roomRepository.save(roomVO);
     }
 
     @Override
     public void update(RoomVO roomVO) {
-
+        roomRepository.save(roomVO);
     }
 
+    // 회원 탈퇴하면 같이 삭제
     @Override
     public void delete(Long aLong) {
 
