007e30be8ad113f346d93984bed4aa0b0d8d2b67
choiseonyoung <bviibb@naver.com>
2021-12-14 14:21:23 +0900

    미니홈피 설정 진행중

--- server/src/main/java/com/roomy/controller/MainController.java
+++ server/src/main/java/com/roomy/controller/MainController.java
@@ -1,16 +1,22 @@
 package com.roomy.controller;
 
 import com.roomy.model.BoardImageVO;
+import com.roomy.model.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
 import java.util.List;
 
 @RestController
 public class MainController {
 
     @GetMapping( {"/",""})
-    public String Main(){
+    public String Main(HttpSession httpSession){
+
+        User user = User.builder().userId("sy").userPassword("1234").userEmail("bviibb@naver.com").userGender("W").userBirth("1997-07-29").userName("최선영").build();
+        httpSession.setAttribute("MEMBER",user);
+
         return "Main Page";
     }
 
