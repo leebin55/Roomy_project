1feec3a4c230c734ac4c63c157a148280b705a6a
1zzang-sm <captain1152@naver.com>
2021-12-15 10:46:29 +0900

    상태관리 로컬스토리지

--- front_ex/src/components/room/todo/TodoMain.jsx
+++ front_ex/src/components/room/todo/TodoMain.jsx
@@ -2,13 +2,19 @@ import React from "react";
 import TodoForm from "./TodoForm";
 import TodoList from "./TodoList";
 import "../../../css/todo/TodoMain.css";
+import { useNavigate } from "react-router-dom";
 // import { useTodoContext } from "../../../context/TodoContextProvider";
 
 function TodoMain() {
+  const navigate = useNavigate();
+  const onClick = () => {
+    navigate("/");
+  };
   return (
     <div className="mainContainer">
       <div className="headerContainer">
-        <h1>TODO LIST</h1>
+        <h1 onClick={onClick}>TODO LIST</h1>
+
         <p>write down all the things to do!</p>
       </div>
       <div className="formContainer">
