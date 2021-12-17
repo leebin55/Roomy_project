2ae94c417044b85af19e4aa704fec193cc33c0c8
leebin55 <yublee55@gmail.com>
2021-12-17 14:47:57 +0900

    초기 더미데이터 수정 '

--- server/src/main/resources/data.sql
+++ server/src/main/resources/data.sql
@@ -1,10 +1,10 @@
-INSERT INTO tbl_board (board_user_seq,board_title,board_content,board_create_at
+INSERT INTO tbl_board (board_user_id,board_title,board_content,board_create_at
                       ,board_code,board_like,board_hit )
-VALUES( 1,'첫번째 게시물','여기는 첫번째 게시물 테스트입니다.','2021-11-30',1,0,10);
+VALUES( 'testid','첫번째 게시물','여기는 첫번째 게시물 테스트입니다.','2021-11-30',1,0,10);
 
-INSERT INTO tbl_board (board_user_seq,board_title,board_content,board_create_at,board_code
+INSERT INTO tbl_board (board_user_id,board_title,board_content,board_create_at,board_code
 ,board_like,board_hit
-) VALUES( 1,'두번째 게시물','여기는 두번째~~ 게시물 테스트입니다.','2021-11-30',1,0,0);
+) VALUES( 'testid','두번째 게시물','여기는 두번째~~ 게시물 테스트입니다.','2021-11-30',1,0,0);
 
 INSERT INTO tbl_board_image (img_board_seq,img_url)
 VALUES( 1,'https://images.unsplash.com/photo-1522770179533-24471fcdba45');
