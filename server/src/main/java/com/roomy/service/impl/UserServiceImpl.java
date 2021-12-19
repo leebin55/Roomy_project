f532cdee8bec35fb8d4674e6a355da745a59f467
zzang <captain1152@naver.com>
2021-12-19 18:22:32 +0900

    findId

--- server/src/main/java/com/roomy/service/impl/UserServiceImpl.java
+++ server/src/main/java/com/roomy/service/impl/UserServiceImpl.java
@@ -7,9 +7,11 @@ import com.roomy.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
-@Service
+@Service("userService")
 @Slf4j
 public class UserServiceImpl implements UserService {
 
@@ -43,5 +45,18 @@ public class UserServiceImpl implements UserService {
     @Override
     public void delete(String s) {
 
+    }
+
+
+    @Override
+    public Optional<User> findByUserName(String userName, String userBirth) {
+        //List<User> member = userRepository.findByUserNameAndUserBirth(user.getUserName(), user.getUserBirth());
+
+        // Optional로 input에 적은 username, userbirth 가져와서 2개 데이터가 일치한것만 보여줌 아니면 정보 없음임
+        // db에 저장된게  username과 userbirth 둘다 일치해야 뜸 아니면 정보없음 임
+        Optional<User> member = Optional.ofNullable(userRepository.findByUserNameAndUserBirth(userName, userBirth)
+                .orElseThrow(() -> new IllegalArgumentException("정보 없음")));
+
+        return member;
     }
 }
